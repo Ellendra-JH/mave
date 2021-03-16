e45c3f6d5555349cfe9e0e8166daf9008fcd2407
Your Name <you@example.com>
2021-03-15 20:11:31 -0400

    product refresh works

--- Javascript/ProductPageJS.js
+++ Javascript/ProductPageJS.js
@@ -1,4 +1,33 @@
-var y = document.getElementById("moredesc");
+
+ 
+
+
+ window.onbeforeunload = function() {
+  localStorage.setItem("theqty", document.getElementById("qty").value);
+  localStorage.setItem("thesize", document.getElementById("size").value);
+  
+
+}
+
+ window.onload = function() {
+  document.getElementById("qty").value = localStorage.getItem("theqty");
+  document.getElementById("size").value = localStorage.getItem("thesize");
+  
+ let y = document.getElementById("moredesc");
+
+ let z = document.getElementById("prices");
+ let nn = document.getElementById("prices").dataset.value *document.getElementById("qty").value*document.getElementById("size").value;
+ z.innerHTML = nn.toFixed(2) + "$";
+ 
+if (document.getElementById("qty").value == null)
+document.getElementById("qty").value = "1"
+if (document.getElementById("size").value == null)
+document.getElementById("size").value = "1"
+
+}
+
+
+
 
 
 
@@ -16,6 +45,7 @@ document.getElementById("qty").onclick = function() {
   z = document.getElementById("prices");
   let nn = document.getElementById("prices").dataset.value *document.getElementById("qty").value*document.getElementById("size").value;
   z.innerHTML = nn.toFixed(2) + "$";
+
 }
 
 document.getElementById("size").onclick = function() {
@@ -26,5 +56,5 @@ document.getElementById("size").onclick = function() {
 
   
   z.innerHTML = nn.toFixed(2) + "$";
-  
+
   }
