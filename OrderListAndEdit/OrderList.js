aa3ee65b25a2fe2b0452581300ea517461beb573
David5102 <61664487+DavidRajotte@users.noreply.github.com>
2021-04-19 09:03:05 -0400

    Dynamic Table

---
+++ OrderListAndEdit/OrderList.js
@@ -0,0 +1,18 @@
+
+function myFunction() {
+    document.getElementById('tabl').deleteRow(0)
+ }
+ 
+ function toDelete(that) {
+            $(that).closest('tr').remove();
+             }
+ 
+ function toAdd() {
+       var row = document.getElementById("samplerow"); 
+       var table = document.getElementById("tabl");
+ var x = document.getElementById("tabl").rows.length;
+ 
+  clone = row.cloneNode(true); 
+  $('table').append(clone);
+     
+     }
